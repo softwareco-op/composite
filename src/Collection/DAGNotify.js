58ed30c7a2c215d23a6a0d40fe18d1a66de0fb00
softwareco-op <seth@softwareco-op.com>
2014-04-29 23:59:12 -0500

    parent and child updates working

--- src/Collection/DAGNotify.js
+++ src/Collection/DAGNotify.js
@@ -26,13 +26,22 @@
     DAGNotify.prototype.add = function(node) {
         var self = this;
         var parent = this.dag.getParent(node);
+        var children = this.dag.getChildren(node);
 
         if (node.object && node.object.add !== undefined) {
             node.object.add(node, this.dag, this.document);
         }
 
-        if (parent && parent.object && parent.object.update !== undefined) {
-            parent.object.update(parent, this.dag, this.document);
+        if (parent && parent.object && parent.object.addChild !== undefined) {
+            parent.object.addChild(node, parent, this.dag, this.document);
+        }
+
+        if (children && children.length > 0) {
+            children.map(function(child) {
+                if (child && child.object && child.object.addParent !== undefined) {
+                    child.object.addParent(node, child, this.dag, this.document);
+                }
+            })
         }
 
         return node;
