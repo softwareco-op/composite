f8c50b5757ccdf9a06ece6b9c8ada808eca3c528
softwareco-op <seth@softwareco-op.com>
2014-06-08 23:52:43 -0500

    Added StoreValue to ballot composition

--- src/Actions/StoreValue.js
+++ src/Actions/StoreValue.js
@@ -2,21 +2,25 @@
 // (C) 2014 SoftwareCo-oP
 ///
 
-(function(COMPOSITE, Global, Action, _) {
+(function(COMPOSITE, Action, DAGUtil, _) {
 
     function StoreValue(node) {this.node = node}
     _.extend(StoreValue.prototype, Action.prototype);
+    COMPOSITE.StoreValue = StoreValue;
 
+    /*
+     * Store the value of a dom element (i.e. input field) on the node.  
+     * Storing the value on the node helps persist data.
+     */
     StoreValue.prototype.perform = function(node, dag) {
         var input = dag.getParent(node);
-        var toChange = this.dag.clone(input);
-        toChange.value = input.object.el.value;
-        this.dag.validateNode(toChange);
+        var toChange = DAGUtil.clone(input);
+        toChange.html.value = input.object.el.value;
+        DAGUtil.validateNode(toChange);
         var head = dag.get('head');
         head.bin.mux.add(toChange);
     }
 
-    COMPOSITE.StoreValue = StoreValue;
     return StoreValue;
 
-})(COMPOSITE, COMPOSITE.Global, COMPOSITE.Action, _);
+})(COMPOSITE, COMPOSITE.Action, COMPOSITE.DAGUtil, _);
